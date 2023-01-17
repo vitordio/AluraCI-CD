c9df0f90e8c068ca28334229d86a084131d85cf3
Vitor Dio <vitor.gracia@gmail.com>
2023-01-17 20:21:55 -0300

    Ajuste na variável de ambiente DB_NAME

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME ") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
+	stringDeConexao := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
